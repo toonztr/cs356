26de72554a4f3d91d7e6dfc720913641950c0f23
Wissarut Junthongpoon <62132299+toonztr@users.noreply.github.com>
2020-09-09 19:03:47 +0700

    New CS356

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by student on 13/9/2562 BE.
+//  Copyright © 2562 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
